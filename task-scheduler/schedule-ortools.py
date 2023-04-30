ff75ced0a70c345ce2278d28a4957e0a6c07bd16
Marine <marine.khornya@gmail.com>
2023-04-30 22:28:12 +0200

    make all intervals optional to prevent no solution found + handle tags

--- task-scheduler/schedule-ortools.py
+++ task-scheduler/schedule-ortools.py
@@ -2,6 +2,7 @@ import collections
 from ortools.sat.python import cp_model
 import pandas as pd
 import math
+from ast import literal_eval
 
 def invert_bit(bit):
     return 1 - bit
@@ -10,11 +11,13 @@ def main():
     # Data.
     tasks = pd.read_csv("data/tasks.csv", sep=';', dtype={'DueDate': 'Int32', 'MaxDueDate': 'Int32'})
     unavailabilities = pd.read_csv("data/unavailabilities.csv", sep=';', dtype={'Start': 'Int32', 'End': 'Int32'})
+    reserved_tags = pd.read_csv("data/tags.csv", sep=';', dtype={'Start': 'Int32', 'End': 'Int32'})#.groupby('Tag')[['Start','End']].apply(lambda x: x.values.tolist())
 
     impacts = list(tasks["Impact"].fillna(0))
     durations = list(tasks["Duration"])
     dueDates = list(tasks['DueDate'])
     maxDueDates = list(tasks['MaxDueDate'])
+    tags = list(tasks['Tags'].apply(literal_eval))
 
     horizon = max(tasks['MaxDueDate'].max(), 1000)
     max_raw_priority = math.floor(max([impact*100/durations[task_id] for impact, task_id in enumerate(impacts)]))
@@ -26,8 +29,13 @@ def main():
     task_type = collections.namedtuple('task_type', 'start end is_present interval raw_priority priority delay is_late')
     # Named tuple to manipulate solution information.
     assigned_task_type = collections.namedtuple('assigned_task_type', 'start task duration priority is_present delay is_late')
+    reserved_tag_interval = collections.namedtuple('reserved_tag_interval', 'interval tags')
 
     all_tasks = {}
+
+    reserved_tag_intervals = []
+    for index, row in reserved_tags.iterrows():
+        reserved_tag_intervals.append(reserved_tag_interval(tags= literal_eval(row['Tags']), interval=model.NewIntervalVar(row['Start'], row['End'] - row['Start'], row['End'], f'reserved_interval_{index}')))
 
     for task_id, duration in enumerate(list(durations)):
         suffix = '_%i' % (task_id)
@@ -36,25 +44,27 @@ def main():
         is_present_var = model.NewBoolVar('is_present' + suffix)
         is_late_var = model.NewBoolVar('is_late' + suffix)
         interval_var = model.NewOptionalIntervalVar(start_var, duration, end_var, is_present_var, 'interval' + suffix)
-        if pd.notna(maxDueDates[task_id]):
-            model.Add(is_present_var == 1)
-            if (maxDueDates[task_id] != dueDates[task_id]):
-                delay_var = model.NewIntVar(-100, 100, 'delay' + suffix)
-                model.AddDivisionEquality(delay_var, (dueDates[task_id] - end_var)*100, maxDueDates[task_id] - dueDates[task_id])
-                model.Add(delay_var < 0).OnlyEnforceIf(is_late_var)
-                model.Add(delay_var >= 0).OnlyEnforceIf(is_late_var.Not())
-                delay_var_abs = model.NewIntVar(0,100, 'delay_abs' + suffix)
-                model.AddAbsEquality(delay_var_abs, delay_var)
-                opt_delay_var = model.NewIntVar(0,100, 'opt_delay' + suffix)
-                model.AddMultiplicationEquality(opt_delay_var, [is_present_var, delay_var_abs])
+        if pd.notna(maxDueDates[task_id]) and maxDueDates[task_id] != dueDates[task_id]:
+            delay_var = model.NewIntVar(-100, 100, 'delay' + suffix)
+            model.AddDivisionEquality(delay_var, (dueDates[task_id] - end_var)*100, maxDueDates[task_id] - dueDates[task_id])
+            model.Add(delay_var < 0).OnlyEnforceIf(is_late_var)
+            model.Add(delay_var >= 0).OnlyEnforceIf(is_late_var.Not())
+            delay_var_abs = model.NewIntVar(0,100, 'delay_abs' + suffix)
+            model.AddAbsEquality(delay_var_abs, delay_var)
+            opt_delay_var = model.NewIntVar(0,100, 'opt_delay' + suffix)
+            model.AddMultiplicationEquality(opt_delay_var, [is_present_var, delay_var_abs])
         else:
             delay_var = model.NewConstant(0)
             opt_delay_var = model.NewBoolVar('opt_delay' + suffix)
             model.Add(opt_delay_var == is_present_var)
-
         raw_priority = math.floor(impacts[task_id]*100/duration)
         priority_var = model.NewIntVar(0, max_raw_priority * 11 * 100, 'priority' + suffix)
         model.AddMultiplicationEquality(priority_var, [raw_priority * opt_delay_var, 10 + is_late_var.Not() - 2 * is_late_var])
+        incompatible_intervals = []
+        for reserved_interval in reserved_tag_intervals:
+            if not(len([value for value in reserved_interval.tags if value in tags[task_id]])):
+                incompatible_intervals.append(reserved_interval.interval)
+        model.AddNoOverlap(incompatible_intervals + [interval_var])
         all_tasks[task_id] = task_type(start=start_var, end=end_var, is_present=is_present_var, interval=interval_var, raw_priority=raw_priority, priority=priority_var, delay=delay_var, is_late=is_late_var)
     
     unavailable_intervals = []
