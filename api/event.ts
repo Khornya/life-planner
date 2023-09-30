be06017e106139bc472fbdd09e1bd90803daa47f
Marine <marine.khornya@gmail.com>
2023-09-30 08:46:00 +0200

    story#11 try to fix vercel build

---
+++ api/event.ts
@@ -0,0 +1,48 @@
+import type { NextApiRequest, NextApiResponse } from 'next/types'
+import { getServerSession } from 'next-auth/next'
+import { createRouter } from 'next-connect'
+
+import { getGoogleCalendar } from '@/lib/server/api/google/calendar'
+
+import { authOptions } from './auth/[...nextauth]'
+
+const router = createRouter<NextApiRequest, NextApiResponse>()
+
+router.post(async (req, res) => {
+  const { id } = req.body
+  const session = await getServerSession(req, res, authOptions)
+
+  if (!session) {
+    return res.status(401).json({ message: 'You must be logged in.' })
+  }
+
+  const calendar = getGoogleCalendar(session)
+
+  return res.send(
+    await calendar.events.update({
+      calendarId: 'primary',
+      eventId: id,
+      sendUpdates: 'all',
+      requestBody: req.body
+    })
+  )
+})
+
+router.delete(async (req, res) => {
+  const ids: string[] = JSON.parse(decodeURIComponent(req.query.ids as string))
+
+  const session = await getServerSession(req, res, authOptions)
+
+  if (!session) {
+    return res.status(401).json({ message: 'You must be logged in.' })
+  }
+
+  const calendar = getGoogleCalendar(session)
+
+  const deletePromises = ids.map(id => calendar.events.delete({ calendarId: 'primary', eventId: id }))
+  Promise.all(deletePromises)
+
+  return res.status(200).end()
+})
+
+export default router.handler()
